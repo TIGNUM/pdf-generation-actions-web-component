b7d23e78fe627a977d52781909b3f5017bf49ece
Luis <lfergon@gmail.com>
2021-08-23 17:22:52 +0200

    feat: work in progress, element registered but this referenced is not correct

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { defineCustomElement } from 'vue';
 import PdfComponent from './src/components/PdfComponent.ce.vue';
 
-const PrintToPdf = defineCustomElement(PdfComponent)
+const PrintToPdf = defineCustomElement(PdfComponent);
 
-export {PrintToPdf}
-console.log('PdfComponent', PdfComponent)
-console.log('PrintToPdf', PrintToPdf)
+console.log('PdfComponent', PdfComponent);
+console.log('PrintToPdf', PrintToPdf);
 
 export function register() {
-  customElements.define('print-to-pdf', PrintToPdf)
+  customElements.define('print-to-pdf', PrintToPdf);
 }
+
+// It was never called
+register();
