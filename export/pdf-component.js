ecc00c07dfb595ad44d84d49a1de527c28d9f292
David Grajal <dgrabla@gmail.com>
2022-04-12 21:54:59 +0200

    feat: render each page independently, then stich them together
    The current implementation has a problem: When the HTML to render is big enough, the canvas size needed to render the whole lot is bigger than the maximum supported canvas size from the browser and then we get glitches: Pages appear empty, images dissapear.
    
    This compounds with the scale option. We need a high scale to be able to
    render the PDF at a good enough resolution for print, but that makes the
    canvas size grow dramatically.
    
    The solution is render each page independently, store the mini PDFs and
    then stich them all together in a unique PDF.
    
    This is what this commit does.
    
    LIMITATIONS
    - The #element-to-print must have a collection of HTML elements, one per
      page, self contained. That means the styles needed to render each page
      must be on each page container.
    
    ALSO
    - Adds 'progress' events to inform the parent how far in the PDF
      creating process we are.
    
    Link: WEBDEV-2492
    https://tignum.atlassian.net/browse/WEBDEV-2492

--- export/pdf-component.js
+++ export/pdf-component.js
@@ -1,4 +1,6 @@
 import html2pdf from 'html2pdf.js';
+import {PDFDocument} from 'pdf-lib';
+
 import DOMPurify from 'dompurify';
 
 class PrintToPdf extends HTMLElement {
@@ -58,25 +60,31 @@ class PrintToPdf extends HTMLElement {
     element.scrollTop = 0;
   }
 
+  _dispatchProgress(message) {
+    const progressEvent = new CustomEvent('progress', {
+      bubbles: true,
+      composed: true,
+      detail: {message}
+    });
+    this.dispatchEvent(progressEvent);
+  }
+
+  // Copypaste from https://stackoverflow.com/questions/60234692/how-to-merge-two-pdf-files-using-jspdf
+  async mergePdfs(pdfsToMerges) {
+    const mergedPdf = await PDFDocument.create();
+    const actions = pdfsToMerges.map(async pdfBuffer => {
+      const pdf = await PDFDocument.load(pdfBuffer);
+      const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+      copiedPages.forEach((page) => {
+        mergedPdf.addPage(page);
+      });
+    });
+    await Promise.all(actions);
+    return await mergedPdf.save();
+  }
+
   _downloadPdf(event) {
     const elementToPrint = this.shadowRoot.querySelector('#element-to-print');
-
-    // Safari doesn't support huge canvas sizes, we lower the resolution for
-    // Apple devices to make sure the PDF fits on the canvas.
-    const isApple = navigator.vendor.match(/apple/i);
-
-    // Firefox does not support a canvas size as big as chrome
-    const isFirefox = navigator.userAgent.match(/firefox/i);
-
-    // Do not abuse, higher values are glitchy and images may disappear or the
-    // file may fail to render altogether
-    let scale = 4;
-    if (isFirefox) {
-      scale = 3;
-    }
-    if (isApple) {
-      scale = 1;
-    }
 
     if (!elementToPrint) {
       console.warn('The web component has not rendered yet, retrying in 100ms');
@@ -92,18 +100,41 @@ class PrintToPdf extends HTMLElement {
         html2canvas: {
           scrollX: 0,
           scrollY: 0,
-          scale
+          scale: 4
         }
       };
-      html2pdf()
-        .set(options)
-        .from(elementToPrint)
-        .toPdf()
-        .save(event.detail?.fileName || 'file.pdf')
-        .then(() => {
-          const downloadedEvent = new Event('downloaded');
-          this.dispatchEvent(downloadedEvent);
+
+      const pages = [...elementToPrint.children[0].children];
+
+      const pagesPdf = [];
+
+      pages.forEach((page, index) => {
+        const pdf = html2pdf().set(options).from(page).toPdf().get('pdf');
+        const buffer = pdf.output('arraybuffer').then((buffer)=>{
+          this._dispatchProgress(`Rendered page ${index}`);
+          return buffer;
         });
+        pagesPdf.push(buffer);
+      });
+
+      this._dispatchProgress('Loading pages');
+
+      Promise.all(pagesPdf).then(async (pdfBuffers)=>{
+        this._dispatchProgress('Merging pages');
+        const mergedPdf = await this.mergePdfs(pdfBuffers);
+        this._dispatchProgress('Downloading');
+        const finalPdf = URL.createObjectURL(
+          new Blob([mergedPdf]), {type: 'application/pdf'}
+        );
+
+        const anchor = document.createElement('a');
+        anchor.href = finalPdf;
+        anchor.download = event.detail?.fileName || 'file.pdf';
+        anchor.click();
+        const downloadedEvent = new Event('downloaded');
+        this.dispatchEvent(downloadedEvent);
+      });
+
     }
     catch (error) {
       throw new Error(error);
